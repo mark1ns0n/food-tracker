957e4fb5e0329fa4310e1c82b8435a3ae2770943
Ivan Markin <mark1ns0n@outlook.com>
2026-01-15 23:09:34 +0400

    Initial Commit

---
+++ foodtracker/foodtrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  foodtrackerApp.swift
+//  foodtracker
+//
+//  Created by Ivan Markin on 15.01.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct foodtrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
